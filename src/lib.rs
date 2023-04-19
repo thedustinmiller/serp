42e636df3e537c161f3098eff2c7acd542a53d04
Dustin Miller <dustin@3qstudios.com>
2023-04-18 21:09:05 -0400

    basic funcitonality and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,90 @@
 use handlebars::Handlebars;
 use std::collections::HashMap;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn format(template: &String, map: &HashMap<String, String>) -> String {
+	let handlebars = Handlebars::new();
+
+	handlebars.render_template(template, map).unwrap()
 }
 
-pub fn format(template: String, map: HashMap<String, String>) -> String {
-    let handlebars = Handlebars::new();
-
-    handlebars.render_template(&template, &map).unwrap()
-
+#[derive(Debug, PartialEq)]
+pub struct Serp {
+	pub template: String,
+	pub map: HashMap<String, String>,
 }
 
+impl Default for Serp {
+	fn default() -> Self {
+		Self {
+			template: "".to_string(),
+			map: HashMap::new(),
+		}
+	}
+}
 
+impl Serp {
+	pub fn new(template: String, map: HashMap<String, String>) -> Self {
+		Self {
+			template,
+			map,
+		}
+	}
+
+	pub fn push(&mut self, key: String, value: String){
+		self.map.insert(key, value);
+	}
+
+	pub fn format(&self) -> String {
+		format(&self.template, &self.map)
+	}
+}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+	#[test]
+	fn default() {
+		let def = Serp::default();
 
-    #[test]
-    fn print_works() {
-        let template = "Hello, {{name}}!".to_string();
-		let map = HashMap::from([("name".to_string(), "Ning Sun".to_string())]);
+		let explicit = Serp {
+			template: "".to_string(),
+			map: HashMap::new(),
+		};
 
-        assert_eq!(format(template, map), "Hello, Ning Sun!".to_string());
-    }
+		assert_eq!(def, explicit);
+	}
+
+	#[test]
+	fn struct_basic_format() {
+		let s = Serp {
+			template: "{{sample}} {{string}}".to_string(),
+			map: HashMap::from(
+				[
+					("sample".into(), "hello".into()),
+					("string".into(), "world".into())
+				]
+			),
+		};
+		assert_eq!(s.format(), "hello world");
+	}
+
+	#[test]
+	fn struct_push() {
+		let mut s = Serp{
+			template: "{{sample}} {{string}}".to_string(),
+			map: HashMap::new(),
+		};
+		s.push("sample".into(), "hello".into());
+		s.push("string".into(), "world".into());
+		assert_eq!(s.format(), "hello world");
+	}
+
+	#[test]
+	fn basic_format() {
+		let template = "Hello, {{name}}!".to_string();
+		let map = HashMap::from([("name".into(), "Ning Sun".into())]);
+
+		assert_eq!(format(&template, &map), "Hello, Ning Sun!".to_string());
+	}
 }
